54ca9443cd6afde8fbba75139a54b332186e6099
rafa meira <meirarafaela@hotmail.com>
2022-03-21 16:09:51 -0300

    svg page not found descomentado

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -7,14 +7,14 @@ const PageNotFound = () => {
             <section className="notfound">
                 <div>
                     <Link to="/">
-                        {/*<svg height="0.8em" width="0.8em" viewBox="0 0 2 1" preserveAspectRatio="none">*/}
-                        {/*    <polyline*/}
-                        {/*        fill="none"*/}
-                        {/*        stroke="#777777"*/}
-                        {/*        strokeWidth="0.1"*/}
-                        {/*        points="0.9,0.1 0.1,0.5 0.9,0.9"*/}
-                        {/*    />*/}
-                        {/*</svg>*/}
+                        <svg height="0.8em" width="0.8em" viewBox="0 0 2 1" preserveAspectRatio="none">
+                            <polyline
+                                fill="none"
+                                stroke="#777777"
+                                strokeWidth="0.1"
+                                points="0.9,0.1 0.1,0.5 0.9,0.9"
+                            />
+                        </svg>
                         Voltar para página inicial
                     </Link>
                     <div className="background-wrapper">
