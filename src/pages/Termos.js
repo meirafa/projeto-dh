ff3d0169d97acb2333a90bb84dcc3d80d21d1564
rafa meira <meirarafaela@hotmail.com>
2022-03-21 16:32:26 -0300

    button home page not found

--- src/pages/Termos.js
+++ src/pages/Termos.js
@@ -9,47 +9,49 @@ function Termos() {
                 <title>DB | Termos de uso</title>
             </Helmet>
             <main>
-                <section>
+                <section className="bg-black">
                     <div className="container">
                         <TitleBgBlack {...title}/>
-
-                        <div className="termos font-text-1 cor-2">
-                            <h2 className="font-1-l cor-3">1. Política de Estorno</h2>
-                            <p>1.1 Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à
-                                vista
-                                também
-                                aceitamos PIX e Boleto. Aceitamos pagamentos parcelados em todos os cartões de crédito.
-                                Para
-                                pagamentos à vista também aceitamos PIX e Boleto.</p>
-                            <p>1.2 Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à
-                                vista
-                                também
-                                aceitamos PIX e Boleto. Aceitamos pagamentos parcelados em todos os cartões de crédito.
-                                Para
-                                pagamentos à vista também aceitamos PIX e Boleto.</p>
-                            <p>1.3 Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à
-                                vista
-                                também
-                                aceitamos PIX e Boleto. Aceitamos pagamentos parcelados em todos os cartões de crédito.
-                                Para
-                                pagamentos à vista também aceitamos PIX e Boleto.</p>
-                            <h2 className="font-1-l cor-3">2. Conta do Usuário</h2>
-                            <p>2.1 Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à
-                                vista
-                                também
-                                aceitamos PIX e Boleto. Aceitamos pagamentos parcelados em todos os cartões de crédito.
-                                Para
-                                pagamentos à vista também aceitamos PIX e Boleto.</p>
-                            <h2 className="font-1-l cor-3">3. Pagamentos</h2>
-                            <p>3.1 Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à
-                                vista
-                                também
-                                aceitamos PIX e Boleto. Aceitamos pagamentos parcelados em todos os cartões de crédito.
-                                Para
-                                pagamentos à vista também aceitamos PIX e Boleto.</p>
-                        </div>
                     </div>
                 </section>
+
+                <article className="container">
+                    <div className="termos font-text-1 cor-2">
+                        <h2 className="font-1-l cor-3">1. Política de Estorno</h2>
+                        <p>1.1 Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à
+                            vista
+                            também
+                            aceitamos PIX e Boleto. Aceitamos pagamentos parcelados em todos os cartões de crédito.
+                            Para
+                            pagamentos à vista também aceitamos PIX e Boleto.</p>
+                        <p>1.2 Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à
+                            vista
+                            também
+                            aceitamos PIX e Boleto. Aceitamos pagamentos parcelados em todos os cartões de crédito.
+                            Para
+                            pagamentos à vista também aceitamos PIX e Boleto.</p>
+                        <p>1.3 Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à
+                            vista
+                            também
+                            aceitamos PIX e Boleto. Aceitamos pagamentos parcelados em todos os cartões de crédito.
+                            Para
+                            pagamentos à vista também aceitamos PIX e Boleto.</p>
+                        <h2 className="font-1-l cor-3">2. Conta do Usuário</h2>
+                        <p>2.1 Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à
+                            vista
+                            também
+                            aceitamos PIX e Boleto. Aceitamos pagamentos parcelados em todos os cartões de crédito.
+                            Para
+                            pagamentos à vista também aceitamos PIX e Boleto.</p>
+                        <h2 className="font-1-l cor-3">3. Pagamentos</h2>
+                        <p>3.1 Aceitamos pagamentos parcelados em todos os cartões de crédito. Para pagamentos à
+                            vista
+                            também
+                            aceitamos PIX e Boleto. Aceitamos pagamentos parcelados em todos os cartões de crédito.
+                            Para
+                            pagamentos à vista também aceitamos PIX e Boleto.</p>
+                    </div>
+                </article>
             </main>
         </>
     )
