f82356cfa0f396d4f3c12c6a1f78674510abb5a8
rafa meira <meirarafaela@hotmail.com>
2022-03-21 15:52:58 -0300

    page not found

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -11,7 +11,7 @@ const PageNotFound = () => {
                             <polyline
                                 fill="none"
                                 stroke="#777777"
-                                stroke-width="0.1"
+                                strokeWidth="0.1"
                                 points="0.9,0.1 0.1,0.5 0.9,0.9"
                             />
                         </svg>
