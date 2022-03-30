8b35cd380d8460f28e76c58942a32b7523dcfe67
rafa meira <meirarafaela@hotmail.com>
2022-03-30 18:50:09 -0300

    alteracao cards home

--- src/pages/components/cards/CardsBlack.js
+++ src/pages/components/cards/CardsBlack.js
@@ -3,7 +3,7 @@ import {useApis} from '../../../hooks/useApi';
 import {Link} from "react-router-dom";
 
 function CardsBlack() {
-    const [carsResult, isLoading] = useApis('/jsons/popular.json');
+    const [carsResult, isLoading] = useApis('/jsons/apiCars.json');
 
     //resultado pode ser nulo ou vazio ate carregar
     const cars = carsResult?.carsList || [];
@@ -14,30 +14,32 @@ function CardsBlack() {
         <>
             {
                 cars.map((carItem, key) => {
-                    return <React.Fragment key={key}>
-                        <div className="populars">
-                            <div className="populars-imagem">
-                                <img src={carItem.img} alt={carItem.category}/>
-                            </div>
-                            <div className="populars-informacoes">
-                                <div>
-                                    <h3 className="font-h3 cor-0">{carItem.title}</h3>
-                                    <ul className="font-text-1 cor-1">
-                                        <li>
-                                            <img src="/img/icones/rastreador.svg" alt="icon local"/>
-                                            {carItem.localization}
-                                        </li>
-                                        <li>
-                                            <img src="/img/icones/carro.png" alt="icon car model"/>
-                                            {carItem.category}
-                                        </li>
-                                    </ul>
-                                    <p className="font-text-1 cor-1">{carItem.description}</p>
+                    if (key < 8) {
+                        return <React.Fragment key={key}>
+                            <div className="populars">
+                                <div className="populars-imagem">
+                                    <img src={carItem.images[0].urlImage} alt={carItem.category.title}/>
                                 </div>
-                                <Link className="botao" to={carItem.link}>Mais Sobre</Link>
+                                <div className="populars-informacoes">
+                                    <div>
+                                        <h3 className="font-h3 cor-0">{carItem.brand} {carItem.model}</h3>
+                                        <ul className="font-text-1 cor-1">
+                                            <li>
+                                                <img src="/img/icones/rastreador.svg" alt="icon local"/>
+                                                {carItem.city.name}
+                                            </li>
+                                            <li>
+                                                <img src="/img/icones/carro.png" alt="icon car model"/>
+                                                {carItem.category.title}
+                                            </li>
+                                        </ul>
+                                        <p className="font-text-1 cor-1">{carItem.description}</p>
+                                    </div>
+                                    <Link className="botao" to={`/produtos/${carItem.id}`}>Mais Sobre</Link>
+                                </div>
                             </div>
-                        </div>
-                    </React.Fragment>
+                        </React.Fragment>
+                    }
                 })
             }
         </>
