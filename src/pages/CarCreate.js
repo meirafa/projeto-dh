e29e0a287a72e0cb79001bc5fc8f4ddd5a184ec2
Eduardo Marcondes <eduardocmarcondes@gmail.com>
2022-04-20 18:23:33 -0300

    subiu ajustes na criação do produto

--- src/pages/CarCreate.js
+++ src/pages/CarCreate.js
@@ -3,15 +3,23 @@ import TitleBgBlack from "./components/titles/TitleBgBlack";
 import { Helmet } from "react-helmet-async";
 import { Formik, Field, Form } from "formik";
 import InputSelect from "./components/forms/InputSelect";
-import {useUser} from "./context/UserContext";
+import { useUser } from "./context/UserContext";
 import InputSelectCategory from "./components/forms/InputSelectCategory";
-import {appConfig} from "../appConfig";
+import { appConfig } from "../appConfig";
+import { useApis } from "../hooks/useApi";
 
 const CarCreate = () => {
 
     const title = { span: "painel de administração", title: "administração de produtos" }
 
-    const {scheduleLocal} = useUser();
+    const [categoryResult, isLoad] = useApis(appConfig.apiUrl + "/categories");
+    const category = categoryResult || [];
+
+    const [cityResult, isLoading] = useApis(appConfig.apiUrl + "/cities");
+    const city = cityResult || [];
+
+
+    const { scheduleLocal } = useUser();
 
     const images = [
         {
@@ -21,43 +29,43 @@ const CarCreate = () => {
         {
             title: '',
             urlImage: ''
-        }, 
+        },
         {
             title: '',
             urlImage: ''
-        }, 
+        },
         {
             title: '',
             urlImage: ''
-        }, 
+        },
         {
             title: '',
             urlImage: ''
         },
     ];
-    
+
 
     function onSubmit(values) {
         console.log('SUBMIT', values)
         const token = localStorage.getItem('token')
         console.log(token)
-         fetch(appConfig.apiUrl + '/products', {
-             method: 'POST',
-             headers: {
-                 "Content-Type": "application/json;",
-                 "Authorization": `Bearer ${token}`
-             },
-             body: JSON.stringify(values)
-         })
-         .then(res => {
-            if (res.ok){
-                console.log(values);
-                alert("Produto criado com sucesso!")
-            } else {
-                alert("Infelizmente, o produto não pôde ser criado. Por favor, tente novamente mais tarde.")
-            }
-         })
-             
+        fetch(appConfig.apiUrl + '/products', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json;",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify(values)
+        })
+            .then(res => {
+                if (res.ok) {
+                    console.log(values);
+                    alert("Produto criado com sucesso!")
+                } else {
+                    alert("Infelizmente, o produto não pôde ser criado. Por favor, tente novamente mais tarde.")
+                }
+            })
+
     }
 
     return (
@@ -75,61 +83,73 @@ const CarCreate = () => {
 
                 <div className="container">
 
-                <Formik
-                    onSubmit={onSubmit}
-                    initialValues={{
-                        brand: '',
-                        model: '',
-                        cost: '',
-                        category: '',
-                        city: '',
-                        description: '',
-                        // attribute: '',
-                        // images: ''
-                    }}
-                    render={({ values }) => (
-                        <Form className="res-form form">
-                            <div>
-                                <label htmlFor="">Marca do Veículo</label>
-                                <Field name="brand" type="text" placeholder="" required />
-                            </div>
-                            <div>
-                                <label htmlFor="">Modelo do Veículo</label>
-                                <Field name="model" type="text" placeholder="" required />
-                            </div>
-                            <div>
-                                <label htmlFor="">Custo de Locação</label>
-                                <Field name="cost" type="number" placeholder="" required className="cost-field" />
-                            </div>
-                            <div>
-                                <label htmlFor="">Categoria</label>
-                                 {/* <InputSelectCategory name="category"/>  */}
-                                <Field name="category" type="text" placeholder=""
-                                    required />
-                            </div>
-                            <div>
-                                <label htmlFor="city">Cidade</label>
-                                <InputSelect name="city"/>
-                                {/* <Field name="city" type="text" placeholder=""
-                                    required /> */}
-                            </div>
-                            <div>
-                                <label htmlFor="">Descrição</label>
-                                <Field name="description" type="text" required />
-                            </div>
-                            {/* 
+                    <Formik
+                        onSubmit={onSubmit}
+                        initialValues={{
+                            brand: '',
+                            model: '',
+                            cost: '',
+                            idCategory: '',
+                            idCity: '',
+                            description: '',
+                            // attribute: '',
+                            // images: ''
+                        }}
+                        render={({ values }) => (
+                            <Form className="res-form form">
+                                <div>
+                                    <label htmlFor="">Marca do Veículo</label>
+                                    <Field name="brand" type="text" placeholder="" required />
+                                </div>
+                                <div>
+                                    <label htmlFor="">Modelo do Veículo</label>
+                                    <Field name="model" type="text" placeholder="" required />
+                                </div>
+                                <div>
+                                    <label htmlFor="">Custo de Locação</label>
+                                    <Field name="cost" type="number" placeholder="" required className="cost-field" />
+                                </div>
+                                <div>
+                                    <label htmlFor="">Categoria</label>
+                                    <Field as="select" name="idCategory">
+                                        {
+                                            category.map((addItem) => {
+                                                return <option value={addItem.id} key={addItem.id}>
+                                                    {addItem.title}</option>
+                                                // return selectValue;
+                                            })
+                                        }
+                                    </Field>
+                                </div>
+                                <div>
+                                    <label htmlFor="">Cidades</label>
+                                    <Field as="select" name="idCity">
+                                        {
+                                            city.map((addItem) => {
+                                                return <option value={addItem.id} key={addItem.id}>
+                                                    {addItem.name}</option>
+                                                // return selectValue;
+                                            })
+                                        }
+                                    </Field>
+                                </div>
+                                <div>
+                                    <label htmlFor="">Descrição</label>
+                                    <Field name="description" type="text" required />
+                                </div>
+                                {/* 
                             <div>
                                 <label htmlFor="">Atributos</label>
                                 <Field name="attribute" type="text" required />
                             </div> */}
-                            <div>
-                                <label htmlFor="">Imagens</label>
-                                <Field name="images" type="text" required />
-                            </div> 
-                            <button className="botao" type="submit">Criar Produto</button>
-                        </Form>
-                    )}
-                />
+                                <div>
+                                    <label htmlFor="">Imagens</label>
+                                    {/* <Field name="images" type="text" required /> */}
+                                </div>
+                                <button className="botao" type="submit">Criar Produto</button>
+                            </Form>
+                        )}
+                    />
                 </div>
 
             </main>
--- src/pages/components/forms/InputSelectCategory.js
+++ src/pages/components/forms/InputSelectCategory.js
@@ -1,33 +1,56 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import Select from 'antd/lib/select';
-import {useApis} from "../../../hooks/useApi";
-import {useUser} from "../../context/UserContext";
+import { useApis } from "../../../hooks/useApi";
+import { useUser } from "../../context/UserContext";
+import { appConfig } from '../../../appConfig';
+import CarCreate from '../../CarCreate';
 
-const {Option} = Select;
+const { Option } = Select;
 
 function InputSelectCategory() {
-    const {scheduleCategory, setScheduleCategory} = useUser();
 
-// Bloco filtro por InputSelect
-    const [categoryResult, isLoad] = useApis('/jsons/apiCars.json');
+    // Bloco filtro por InputSelect
+    const [categoryResult, isLoad] = useApis(appConfig.apiUrl + "/categories");
 
     //resultado pode ser nulo ou vazio ate carregar
-    const category = categoryResult?.carsList || [];
+    const category = categoryResult || [];
 
     function enviandoValor(value) {
-        setScheduleCategory(value);
+        console.log(value)
+
+        // setScheduleCategory(value);
     }
 
-    const set = new Set([]);
-    let newSet = [];
+    // let selectValue = {};
+
+    // const set = new Set([]);
+    // let newSet = [...selectValue];
+
+    /* React.useEffect(() => {
+
+        fetch(appConfig.apiUrl + '/categories', {
+             method: 'GET',
+             headers: {
+                 "Content-Type": "application/json;"
+             },
+         })
+            // conventendo o http response em um objeto json
+            .then(res => {
+                console.log(res)
+                if (res.ok) {
+                    return res.json()
+                } 
+            })
+
+    },[]) */
 
     return (
         <>
             {!!isLoad ? 'Carregando...' : null}
             <Select
                 showSearch
-                style={{minWidth: 250}}
+                style={{ minWidth: 250 }}
                 placeholder="Selecionar Categoria"
                 optionFilterProp="children"
                 filterOption={(input, option) =>
@@ -38,20 +61,17 @@ function InputSelectCategory() {
                 }
                 size="large"
                 onSelect={enviandoValor}
-                defaultValue={scheduleCategory}
+            // defaultValue={scheduleCategory}
             >
                 {
                     category.map((addItem) => {
-                        set.add(addItem.category.title);
-                        return newSet = [...set];
+                        return <Option value={addItem.id} key={addItem.id}>
+                        {addItem.title}</Option>
+                        // return selectValue;
                     })
                 }
                 {
-                    newSet.map((item, key) => {
-                        return <Option
-                            value={item}
-                            key={key}>{item}</Option>
-                    })
+                    
                 }
             </Select>
         </>)
